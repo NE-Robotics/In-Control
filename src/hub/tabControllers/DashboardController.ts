e8f493e953223d55c861e52de46bf204bd76fae8
Autumn <62668093+Autumn-Ou@users.noreply.github.com>
2023-02-25 23:53:11 -0500

    Dashboard cam swapper

--- src/hub/tabControllers/DashboardController.ts
+++ src/hub/tabControllers/DashboardController.ts
@@ -1,37 +1,28 @@
 import { TabState } from "../../packages/utils/HubState";
-import LogFieldTree from "../../packages/log/LogFieldTree";
 import TabType from "../../packages/utils/TabType";
-import { arraysEqual } from "../../packages/utils/util";
 import TabController from "../TabController";
-import LogField from "../../packages/log/LogField";
-import getBoolean from "../../packages/log/Log";
 import { getIsRedAlliance, getEnabledData, getFMSControlData } from "../../packages/log/LogUtil";
 
 export default class DashboardController implements TabController {
-  private NO_DATA_ALERT: HTMLElement;
-  private TABLE_CONTAINER: HTMLElement;
-  private TABLE_BODY: HTMLElement;
   private VIDEO_CONTAINER: HTMLElement;
-
-  private fields: { [id: string]: LogField } = {};
+  private SOURCE_SELECTOR: HTMLInputElement;
+  private port: string | undefined;
 
   private teleop = [32, 33, 48, 49];
   private autonomous = [34, 35, 50, 51];
   private test = [36, 37, 52, 53];
-  private FMSConnected = [0, 1, 16, 17];
-
-  private isRed: any;
-
-  private key: any;
 
   private content: HTMLElement;
-
   constructor(content: HTMLElement) {
     this.content = content;
-    this.NO_DATA_ALERT = content.getElementsByClassName("tab-centered")[0] as HTMLElement;
-    this.TABLE_CONTAINER = content.getElementsByClassName("dashboard-table-container")[0] as HTMLElement;
-    this.TABLE_BODY = content.getElementsByClassName("dashboard-table")[0].firstElementChild as HTMLElement;
     this.VIDEO_CONTAINER = content.getElementsByClassName("dashboard-video-container")[0] as HTMLElement;
+    this.SOURCE_SELECTOR = content.getElementsByClassName("dashboard-video-source-selector")[0] as HTMLInputElement;
+    this.SOURCE_SELECTOR.addEventListener("change", () => {
+      this.port = this.SOURCE_SELECTOR.value;
+      this.connectStream(this.port);
+    });
+    // Cam Handle
+    this.connectStream(this.port ?? "1181");
   }
 
   saveState(): TabState {
@@ -80,12 +71,17 @@ export default class DashboardController implements TabController {
     } else {
       this.content.getElementsByClassName("isFMSConnected")[0].innerHTML = "False";
     }
+  }
 
-    // Cam Handle
+  connectStream(port: string) {
+    console.log("Connecting to stream on port " + port);
     if (window.preferences?.dashboardMode == "sim") {
-      this.VIDEO_CONTAINER.setAttribute("src", "http://127.0.0.1:1181/stream.mjpg");
+      this.VIDEO_CONTAINER.setAttribute("src", "http://127.0.0.1:" + port + "/stream.mjpg");
     } else if (window.preferences?.dashboardMode == "real") {
-      this.VIDEO_CONTAINER.setAttribute("src", "http://" + window.preferences?.rioAddress + ":1181/stream.mjpg");
+      this.VIDEO_CONTAINER.setAttribute(
+        "src",
+        "http://" + window.preferences?.rioAddress + ":" + port + "/stream.mjpg"
+      );
     } else {
       this.VIDEO_CONTAINER.style.display = "none";
     }
--- www/docs/tabs/DASHBOARD.md
+++ www/docs/tabs/DASHBOARD.md
@@ -2,8 +2,15 @@
 
 _[< Return to homepage](/docs/INDEX.md)_
 
-The dashboard view is designed to display basic FMS data along with the first camera feed by default.
+The dashboard view is designed to display basic FMS data along with your camera feed of choice
+
+## Camera Feed
 
 - **Default Cam IP & Port** - 10.TE.AM.2:1181
+- Video Stream dropdown one - nine ports 1181 to 1189
 
-  UPDATE AFTER CUSTOM SYSTEM ADDED TO PREFS TO SUPPORT VLANS & NONSTANDARD CAM SYSTEMS
+## FMS Data Table
+
+- Mode, shows current mode, along with enabled & disabled
+- FMS, true or false if FMS is connected
+- Allaince, current allaince colour
